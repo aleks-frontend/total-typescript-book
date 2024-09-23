1b124b740fdbc46c9be9838d1b6db140682cfd23
Aleksandar Gojkovic <aleksfrontend@gmail.com>
2024-09-23 16:52:35 +0200

    initial

--- src/005-kickstart-your-typescript-setup/001-typescript-vs-javascript.explainer.ts
+++ src/005-kickstart-your-typescript-setup/001-typescript-vs-javascript.explainer.ts
@@ -1,5 +1,5 @@
 // Explain autocomplete and type checking
-
+let element = document.getElementById('12');
 // Check your types are correct across your whole application
 
 // Refactor with confidence
